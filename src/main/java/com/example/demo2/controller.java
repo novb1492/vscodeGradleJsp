e81168091df62d6deb1726fe675e7be77a4d6353
novb1492 <novb1492@naver.com>
2021-07-18 13:46:52 +0900

    --

---
+++ src/main/java/com/example/demo2/controller.java
@@ -0,0 +1,15 @@
+package com.example.demo2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+    
+    @GetMapping("/")
+    public String main(){
+
+        return "main";
+
+    }
+}
